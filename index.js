d805fb44bd800a17b193773d07b46c0d3c2f75de
Zulai Romero <zulairomero@gmail.com>
2023-04-09 13:15:30 -0400

    adds ccompleted lab for modifying the properties of objects

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution in this file!
+const employee = {
+    name: "Kevin",
+    streetAddress: "102 Maple Street",
+};
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return {
+        ...employee,
+        [key] : value,
+    } 
+};
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(
+        employee,
+        "name",
+        "Josh",
+    );
+
+console.log(updatedEmployee);
+
+    // -----------------------------------------------------
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    object[key] = value;
+    return object;
+};
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "streetAddress", "Herald Square");
+
+// ----------------------------------------------------------------
+
+function deleteFromEmployeeByKey(object, key) {
+    const newEmployee = {...object};
+    delete newEmployee[key];
+    return newEmployee;
+};
+
+deleteFromEmployeeByKey(employee, "name");
+
+// --------------------------------------------------------------
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+};
+
+destructivelyDeleteFromEmployeeByKey(employee, "name");
